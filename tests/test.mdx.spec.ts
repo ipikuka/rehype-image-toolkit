2968613739788b9ef0f0c3919c4140d720919ea5
talatkuyuk <talatkuyuk@gmail.com>
2025-04-04 22:16:55 +0300

    Add a mdx source test to be fixed

--- tests/test.mdx.spec.ts
+++ tests/test.mdx.spec.ts
@@ -6,6 +6,40 @@ import { processMdx } from "./util/reference.mdx";
 
 describe("reyhpe-image-hack, with MDX sources", () => {
   // ******************************************
+  it("MDX source, basic auto link", async () => {
+    const input = dedent`
+      ![]([image.png])
+
+      ![]({image.png})
+
+      <img src="[image.png]" alt="" />
+
+      <img src="{image.png}" alt="" />
+    `;
+
+    const html = await processMdx(input, "mdx");
+
+    expect(html).toMatchInlineSnapshot(`
+      "<p><img src="%5Bimage.png%5D" alt=""/></p>
+      <p><img src="%7Bimage.png%7D" alt=""/></p>
+      <img src="[image.png]" alt=""/>
+      <img src="{image.png}" alt=""/>"
+    `);
+
+    expect(await prettier.format(html, { parser: "mdx" })).toMatchInlineSnapshot(`
+      "<p>
+        <img src="%5Bimage.png%5D" alt="" />
+      </p>
+      <p>
+        <img src="%7Bimage.png%7D" alt="" />
+      </p>
+      <img src="[image.png]" alt="" />
+      <img src="{image.png}" alt="" />
+      "
+    `);
+  });
+
+  // ******************************************
   it("Basic MDX source", async () => {
     const input = dedent`
       ![](image.png)
