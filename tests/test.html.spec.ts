e39fd89d6f0ec8cc0c90e47023107886cc1d657c
talatkuyuk <talatkuyuk@gmail.com>
2025-04-05 06:59:30 +0300

    Refactor html source tests

--- tests/test.html.spec.ts
+++ tests/test.html.spec.ts
@@ -1,10 +1,98 @@
 import { describe, it, expect } from "vitest";
 import dedent from "dedent";
-// import * as prettier from "prettier";
+import * as prettier from "prettier";
 
 import { processHtml } from "./util";
 
 describe("reyhpe-image-hack, with html sources", () => {
+  // *************************************
+  it("process html input, preserve simple image", async () => {
+    const input = dedent`
+      <img src="image.png" alt="alt">
+    `;
+
+    const html = String(await processHtml(input));
+
+    expect(html).toMatchInlineSnapshot(`"<img src="image.png" alt="alt">"`);
+  });
+
+  // *************************************
+  it("process html input, handle figure and caption", async () => {
+    const input = dedent`
+      <img src="image.png" alt="*Image Caption">
+      <img src="image.png" alt="+Image Alt">
+    `;
+
+    const html = String(await processHtml(input));
+
+    expect(await prettier.format(html, { parser: "html" })).toMatchInlineSnapshot(`
+      "<figure>
+        <img src="image.png" alt="Image Caption" />
+        <figcaption>Image Caption</figcaption>
+      </figure>
+      <figure><img src="image.png" alt="Image Alt" /></figure>
+      "
+    `);
+  });
+
+  // *************************************
+  it("process html input, handle figure and caption in paragraph", async () => {
+    const input = dedent`
+      <p><img src="image.png" alt="*Image Caption"><img src="image.png" alt="+Image Alt"></p>
+    `;
+
+    const html = String(await processHtml(input));
+
+    expect(await prettier.format(html, { parser: "html" })).toMatchInlineSnapshot(`
+      "<figure>
+        <img src="image.png" alt="Image Caption" />
+        <figcaption>Image Caption</figcaption>
+      </figure>
+      <figure><img src="image.png" alt="Image Alt" /></figure>
+      "
+    `);
+  });
+
+  // *************************************
+  it("process html input, handle indentations and whitespaces", async () => {
+    const input = dedent`
+      <h4>Title</h4>
+      <p>
+        <img src="image.png" alt="*Image Caption">
+      </p>
+      <img src="image.png" alt="+Image Alt">
+    `;
+
+    const html = String(await processHtml(input));
+
+    expect(html).toMatchInlineSnapshot(`
+      "<h4>Title</h4>
+      <figure><img src="image.png" alt="Image Caption"><figcaption>Image Caption</figcaption></figure>
+      <figure><img src="image.png" alt="Image Alt"></figure>"
+    `);
+  });
+
+  // *************************************
+  it("process html input, handle autolink", async () => {
+    const input = dedent`
+      <img src="[image.png]" alt="alt">
+      <img src="(image.png)" alt="alt">
+
+      <img src="[image.png]" alt="*caption">
+      <img src="(image.png)" alt="*caption">
+    `;
+
+    const html = String(await processHtml(input));
+
+    expect(html).toMatchInlineSnapshot(`
+      "<a href="image.png" target="_blank"><img src="image.png" alt="alt"></a>
+      <a href="image.png" target="_blank"><img src="image.png" alt="alt"></a>
+
+      <a href="image.png" target="_blank"><figure><img src="image.png" alt="caption"><figcaption>caption</figcaption></figure></a>
+      <figure><a href="image.png" target="_blank"><img src="image.png" alt="caption"></a><figcaption>caption</figcaption></figure>"
+    `);
+  });
+
   // *************************************
   it("process html input, example in the README", async () => {
     const input = dedent`
@@ -37,73 +125,6 @@ describe("reyhpe-image-hack, with html sources", () => {
         It adds attributes.
         <img src="image.png" title="title" width="60" height="60">
       </p>"
-    `);
-  });
-});
-
-describe("reyhpe-image-hack, simple", () => {
-  const output =
-    '"<figure><img src="image.png" alt="Image Caption"><figcaption>Image Caption</figcaption></figure>"';
-
-  // *************************************
-  it("process html input, simple image", async () => {
-    const input = dedent`
-      <img src="image.png" alt="*Image Caption">
-    `;
-
-    const html = String(await processHtml(input));
-
-    expect(html).toMatchInlineSnapshot(output);
-  });
-
-  // *************************************
-  it("process html input, example in the README", async () => {
-    const input = dedent`
-      <p><img src="image.png" alt="*Image Caption"></p>
-    `;
-
-    const html = String(await processHtml(input));
-
-    expect(html).toMatchInlineSnapshot(output);
-  });
-
-  // *************************************
-  it("process html input, preserve whitespaces", async () => {
-    const input = dedent`
-      <h4>Title</h4>
-      <p>
-        <img src="image.png" alt="*Image Caption">
-      </p>
-      <img src="image.png" alt="+Image Alt">
-    `;
-
-    const html = String(await processHtml(input));
-
-    expect(html).toMatchInlineSnapshot(`
-      "<h4>Title</h4>
-      <figure><img src="image.png" alt="Image Caption"><figcaption>Image Caption</figcaption></figure>
-      <figure><img src="image.png" alt="Image Alt"></figure>"
-    `);
-  });
-
-  // *************************************
-  it("process html input, add autolink", async () => {
-    const input = dedent`
-      <img src="[image.png]" alt="alt">
-      <img src="(image.png)" alt="alt">
-
-      <img src="[image.png]" alt="*caption">
-      <img src="(image.png)" alt="*caption">
-    `;
-
-    const html = String(await processHtml(input));
-
-    expect(html).toMatchInlineSnapshot(`
-      "<a href="image.png" target="_blank"><img src="image.png" alt="alt"></a>
-      <a href="image.png" target="_blank"><img src="image.png" alt="alt"></a>
-
-      <a href="image.png" target="_blank"><figure><img src="image.png" alt="caption"><figcaption>caption</figcaption></figure></a>
-      <figure><a href="image.png" target="_blank"><img src="image.png" alt="caption"></a><figcaption>caption</figcaption></figure>"
     `);
   });
 });
