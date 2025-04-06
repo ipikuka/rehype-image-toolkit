4a3907af51a75b2a3e1f2c0302e9ad4ad4a3b22c
talatkuyuk <talatkuyuk@gmail.com>
2025-04-06 07:02:22 +0300

    Add tests for autolink for MDX sources

--- tests/test.mdx.spec.ts
+++ tests/test.mdx.spec.ts
@@ -1,72 +1,79 @@
 import { describe, it, expect } from "vitest";
 import dedent from "dedent";
-import * as prettier from "prettier";
+// import * as prettier from "prettier";
 
-import { processMdx } from "./util/reference.mdx";
+import { processMdx, processMdxRaw } from "./util/index.mdx";
 
 describe("reyhpe-image-hack, with MDX sources", () => {
   // ******************************************
-  it("MDX source, handle autolink", async () => {
-    const input = dedent`
-      ![]([image.png])
-
-      ![]((image.png))
-
-      <img src="[image.png]" alt="" />
-
-      <img src="(image.png)" alt="" />
-    `;
-
-    const html = await processMdx(input, "mdx");
-
-    expect(html).toMatchInlineSnapshot(`
-      "<p><img src="%5Bimage.png%5D" alt=""/></p>
-      <p><img src="(image.png)" alt=""/></p>
-      <img src="[image.png]" alt=""/>
-      <img src="(image.png)" alt=""/>"
-    `);
-
-    const md = await processMdx(input, "md");
-
-    expect(md).toMatchInlineSnapshot(`
-      "<p><img src="%5Bimage.png%5D" alt=""/></p>
-      <p><img src="(image.png)" alt=""/></p>"
-    `);
-  });
-
-  // ******************************************
   it("Basic MDX source", async () => {
     const input = dedent`
+      <h2>Hi</h2>
+
       ![](image.png)
 
       <img src="image.png" alt="" />
     `;
 
-    const html = await processMdx(input, "mdx");
-
-    expect(html).toMatchInlineSnapshot(`
-      "<p><img src="image.png" alt=""/></p>
+    expect(await processMdxRaw(input, "md")).toMatchInlineSnapshot(`
+      "<h2>Hi</h2>
+      <p><img src="image.png" alt=""/></p>
       <img src="image.png" alt=""/>"
     `);
 
-    expect(await prettier.format(html, { parser: "mdx" })).toMatchInlineSnapshot(`
-      "<p>
-        <img src="image.png" alt="" />
-      </p>
-      <img src="image.png" alt="" />
-      "
+    expect(await processMdx(input, "mdx")).toMatchInlineSnapshot(`
+      "<h2>Hi</h2>
+      <p><img src="image.png" alt=""/></p>
+      <img src="image.png" alt=""/>"
+    `);
+  });
+
+  // TODO handle mdxJsxFlowElement
+  // ******************************************
+  it("MDX source, handle autolinks", async () => {
+    const input = dedent`
+      ![]([image.png])
+
+      ![]((image.png))
+
+      handle ![]([image.png]) ![]((image.png)) in a paragraph
+
+      <img src="[image.png]" alt="" />
+
+      <img src="(image.png)" alt="" />
+
+      <img src="[image.png]" alt="" /> text
+
+      <img src="(image.png)" alt="" /> text
+
+      handle <img src="[image.png]" alt="" /> <img src="(image.png)" alt="" /> in a paragraph
+    `;
+
+    expect(await processMdxRaw(input, "md")).toMatchInlineSnapshot(`
+      "<p><a href="image.png" target="_blank"><img src="image.png" alt=""/></a></p>
+      <p><a href="image.png" target="_blank"><img src="image.png" alt=""/></a></p>
+      <p>handle <a href="image.png" target="_blank"><img src="image.png" alt=""/></a> <a href="image.png" target="_blank"><img src="image.png" alt=""/></a> in a paragraph</p>
+      <a href="image.png" target="_blank"><img src="image.png" alt=""/></a>
+      <a href="image.png" target="_blank"><img src="image.png" alt=""/></a>
+      <p><a href="image.png" target="_blank"><img src="image.png" alt=""/></a> text</p>
+      <p><a href="image.png" target="_blank"><img src="image.png" alt=""/></a> text</p>
+      <p>handle <a href="image.png" target="_blank"><img src="image.png" alt=""/></a> <a href="image.png" target="_blank"><img src="image.png" alt=""/></a> in a paragraph</p>"
+    `);
+
+    expect(await processMdx(input, "mdx")).toMatchInlineSnapshot(`
+      "<p><a href="image.png" target="_blank"><img src="image.png" alt=""/></a></p>
+      <p><a href="image.png" target="_blank"><img src="image.png" alt=""/></a></p>
+      <p>handle <a href="image.png" target="_blank"><img src="image.png" alt=""/></a> <a href="image.png" target="_blank"><img src="image.png" alt=""/></a> in a paragraph</p>
+      <a href="image.png" target="_blank"><img src="image.png" alt=""/></a>
+      <a href="image.png" target="_blank"><img src="image.png" alt=""/></a>
+      <p><a href="image.png" target="_blank"><img src="image.png" alt=""/></a> text</p>
+      <p><a href="image.png" target="_blank"><img src="image.png" alt=""/></a> text</p>
+      <p>handle <a href="image.png" target="_blank"><img src="image.png" alt=""/></a> <a href="image.png" target="_blank"><img src="image.png" alt=""/></a> in a paragraph</p>"
     `);
   });
 });
 
 // for reference
-
-// const element = {
-//   type: "element",
-//   tagName: "img",
-//   properties: { src: "image.png", alt: "" },
-//   children: [],
-// };
 
 // const mdxJsxFlowElement = {
 //   type: "mdxJsxFlowElement",
