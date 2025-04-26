2ea9b89953c25c38afd4316021f08c3bb7131794
talatkuyuk <talatkuyuk@gmail.com>
2025-04-26 16:17:34 +0300

    Add two tests for new options in md and mdx tests

--- tests/test.md.spec.ts
+++ tests/test.md.spec.ts
@@ -1367,6 +1367,57 @@ describe("reyhpe-image-hack, with markdown sources", () => {
   });
 
   // ******************************************
+  it("disable features ExplicitFigure and ExplicitAutolink", async () => {
+    const input = dedent`
+      ![^alt]([image.png])
+
+      <p><img src="[image.png]" alt="^alt"></p>
+    `;
+
+    const html = String(
+      await processMdRawFirst(input, { explicitAutolink: false, explicitFigure: false }),
+    );
+
+    expect(html).toMatchInlineSnapshot(`
+      "<p><img src="image.png" alt="alt"></p>
+      <p><img src="image.png" alt="alt"></p>"
+    `);
+  });
+
+  // ******************************************
+  it("images in other elements", async () => {
+    const input = dedent`
+      + xxx ![~](image.png)
+      + xxx ![&](image.png)
+      + xxx ![^](image.png)
+
+      # Title ![~](image.png)
+      # Title ![&](image.png)
+      # Title ![^](image.png)
+
+      **![&](image.png)**
+      **![~](image.png)**
+      **![^](image.png)**
+    `;
+
+    const html = String(await processMd(input));
+
+    expect(html).toMatchInlineSnapshot(`
+      "<ul>
+      <li>xxx <img src="image.png" alt=""></li>
+      <li>xxx <img src="image.png" alt=""></li>
+      <li>xxx <figure><img src="image.png" alt=""></figure></li>
+      </ul>
+      <h1>Title <img src="image.png" alt=""></h1>
+      <h1>Title <img src="image.png" alt=""></h1>
+      <h1>Title <figure><img src="image.png" alt=""></figure></h1>
+      <p><strong><img src="image.png" alt=""></strong>
+      <strong><img src="image.png" alt=""></strong>
+      <strong><figure><img src="image.png" alt=""></figure></strong></p>"
+    `);
+  });
+
+  // ******************************************
   it("example in the README", async () => {
     const input = dedent`
       It ensures adding videos/audio using image syntax. ![](video.mp4) 
@@ -1378,7 +1429,7 @@ describe("reyhpe-image-hack, with markdown sources", () => {
       It unwraps images from paragraph ![&alt](image.png)
 
       It adds attributes. ![](video.mp4 "title > 640x480 autoplay")
-  `;
+    `;
 
     const html = String(await processMd(input));
 
@@ -1415,39 +1466,6 @@ describe("reyhpe-image-hack, with markdown sources", () => {
       <img src="image.png" alt="alt">
       <p>It adds attributes.</p>
       <video title="title" width="640" height="480" autoplay><source src="video.mp4" type="video/mp4"></video>"
-    `);
-  });
-
-  // ******************************************
-  it("images in other elements", async () => {
-    const input = dedent`
-    + xxx ![~](image.png)
-    + xxx ![&](image.png)
-    + xxx ![^](image.png)
-
-    # Title ![~](image.png)
-    # Title ![&](image.png)
-    # Title ![^](image.png)
-
-    **![&](image.png)**
-    **![~](image.png)**
-    **![^](image.png)**
-`;
-
-    const html = String(await processMd(input));
-
-    expect(html).toMatchInlineSnapshot(`
-      "<ul>
-      <li>xxx <img src="image.png" alt=""></li>
-      <li>xxx <img src="image.png" alt=""></li>
-      <li>xxx <figure><img src="image.png" alt=""></figure></li>
-      </ul>
-      <h1>Title <img src="image.png" alt=""></h1>
-      <h1>Title <img src="image.png" alt=""></h1>
-      <h1>Title <figure><img src="image.png" alt=""></figure></h1>
-      <p><strong><img src="image.png" alt=""></strong>
-      <strong><img src="image.png" alt=""></strong>
-      <strong><figure><img src="image.png" alt=""></figure></strong></p>"
     `);
   });
 });
